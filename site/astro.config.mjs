ad5fef065f396d5b876590988915f85a1685bdf5
willow <42willow@pm.me>
2025-04-08 18:41:15 +1000

    feat(site): move to tailwind for styles

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
   site: "https://42willow.github.io",
   base: "wallpapers",
 });
--- site/src/utils/flavour-selector.ts
+++ site/src/utils/flavour-selector.ts
@@ -1,12 +1,22 @@
 console.log("theme selector loaded");
 
 const flavours = ["latte", "frappe", "macchiato", "mocha", "unthemed"];
+let currentTheme = localStorage.getItem("theme") || "macchiato";
 
 flavours.forEach((flavour) => {
   let btn = document.querySelector(
-    `.flavour-selector > .${flavour}`,
+    `#flavour-selector > #${flavour}`,
   ) as HTMLButtonElement;
+  if (!btn) {
+    console.error(`Button with id ${flavour} not found`);
+    return;
+  }
   btn.onclick = () => {
+    console.log("theme selector clicked", flavour);
+    document.querySelectorAll("#flavour-selector > button").forEach((b) => {
+      b.classList.remove("active");
+    });
+    btn.classList.toggle("active");
     document.body.className = flavour;
     localStorage.setItem("theme", flavour);
   };
