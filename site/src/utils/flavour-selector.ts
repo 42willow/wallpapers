a53e95d787223ac8ca0e727dcdff1b41459ef257
willow <42willow@pm.me>
2025-04-08 21:10:48 +1000

    refactor(site): theme selection logic

--- site/src/utils/flavour-selector.ts
+++ site/src/utils/flavour-selector.ts
@@ -1,23 +1,25 @@
-console.log("theme selector loaded");
-
 const flavours = ["latte", "frappe", "macchiato", "mocha", "unthemed"];
-let currentTheme = localStorage.getItem("theme") || "macchiato";
 
+applyTheme("macchiato");
+
+// flavour selector listeners
 flavours.forEach((flavour) => {
   let btn = document.querySelector(
     `#flavour-selector > #${flavour}`,
   ) as HTMLButtonElement;
-  if (!btn) {
-    console.error(`Button with id ${flavour} not found`);
-    return;
-  }
   btn.onclick = () => {
-    console.log("theme selector clicked", flavour);
-    document.querySelectorAll("#flavour-selector > button").forEach((b) => {
-      b.classList.remove("active");
-    });
-    btn.classList.toggle("active");
-    document.body.className = flavour;
-    localStorage.setItem("theme", flavour);
+    applyTheme(flavour);
   };
 });
+
+function applyTheme(theme: string) {
+  document.body.className = theme;
+  // reset active class
+  document.querySelectorAll("#flavour-selector > button").forEach((btn) => {
+    btn.classList.remove("active");
+  });
+  // set active button
+  document
+    .querySelector(`#flavour-selector > #${theme}`)
+    ?.classList.add("active");
+}
